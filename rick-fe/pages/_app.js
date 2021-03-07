b1725b7df86d71f0db591ea36618f77867e72154
Dustin Goodman <dustin.s.goodman@gmail.com>
2021-03-07 16:08:48 -0600

    configure react-query and material ui

--- rick-fe/pages/_app.js
+++ rick-fe/pages/_app.js
@@ -1,7 +1,49 @@
-import '../styles/globals.css'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { QueryClient, QueryClientProvider } from 'react-query'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import GlobalStyle from '../src/shared/global-style';
 
-export default MyApp
+const defaultTitle = 'Rick and Morty';
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
+const queryClient = new QueryClient();
+
+const App = ({ Component, pageProps }) => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <CssBaseline />
+      <Helmet
+        htmlAttributes={{ lang: 'en' }}
+        title={defaultTitle}
+        link={[
+          {
+            rel: 'icon',
+            href: '/favicon.ico',
+          },
+        ]}
+        meta={[
+          {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1',
+          },
+          { property: 'og:title', content: defaultTitle },
+          {
+            name: 'description',
+            content: 'Rick and Morty universe information',
+          },
+        ]}
+      />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </QueryClientProvider>
+);
+
+export default App;
