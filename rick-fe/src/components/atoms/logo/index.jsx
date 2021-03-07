ae8874331ddac2e52b4d7f05cca65c04326d1dc8
Dustin Goodman <dustin.s.goodman@gmail.com>
2021-03-07 16:09:09 -0600

    add logo and typography

---
+++ rick-fe/src/components/atoms/logo/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Logo = () => (
+  <img src="/logo.svg" alt="Rick and Morty Logo" />
+);
+
+export default Logo;
---
+++ rick-fe/src/components/atoms/typography/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MTypography from '@material-ui/core/Typography';
+
+const VARIANTS = {
+  'p': 'body1',
+  'small': 'body2',
+}
+
+const Typography = ({ type, children }) => {
+  const variant = VARIANTS[type] || type;
+  return (
+    <MTypography variant={variant}>
+      {children}
+    </MTypography>
+  );
+};
+
+Typography.propTypes = {
+  type: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'small']),
+};
+Typography.defaultProps = {
+  type: 'p',
+};
+
+export default Typography;
