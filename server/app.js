55cc454104b9de9de6d6eee5dd9d631d8e3c89ae
Dustin Goodman <dustin.s.goodman@gmail.com>
2021-03-21 20:50:17 -0500

    add locations apis

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 4000;
-const { Characters } = require('./models');
+const { Characters, Locations } = require('./models');
 
 app.get('/characters', async (req, resp) => {
   const page = parseInt(req.query?.page ?? 1, 10);
@@ -52,12 +52,53 @@ app.get('/characters/:id', async (req, resp) => {
   });
 });
 
-app.get('/locations', (req, resp) => {
-  resp.send('hello worlds');
+app.get('/locations', async (req, resp) => {
+  const page = parseInt(req.query?.page ?? 1, 10);
+  const perPage = parseInt(req.query?.perPage ?? 20, 10);
+  const [locations, locationsCount] = await Promise.all([
+    Locations.findAll({
+      limit: perPage,
+      offset: (page - 1) * perPage,
+    }),
+    Locations.count()
+  ]);
+
+  resp.append('Access-Control-Allow-Origin', '*');
+  resp.append('Access-Control-Allow-Credentials', true);
+  resp.status(200);
+  resp.send({
+    data: locations,
+    pageInfo: {
+      page: page,
+      perPage: perPage,
+      total: locationsCount,
+      totalPages: Math.ceil(locationsCount / perPage ),
+    }
+  });
 });
 
-app.get('/locations/:id', (req, resp) => {
-  resp.send('hello world');
+app.get('/locations/:id', async (req, resp) => {
+  const id = parseInt(req.params?.id, 10);
+  if (id <= 0) {
+    resp.status(400);
+    resp.send('Invalid ID');
+    return;
+  }
+
+  const location = await Locations.findByPk(id);
+
+  if (!location) {
+    resp.status(404);
+    resp.send('Location does not exist');
+    return;
+  }
+
+  resp.append('Access-Control-Allow-Origin', '*');
+  resp.append('Access-Control-Allow-Credentials', true);
+  resp.status(200);
+  resp.send({
+    data: location,
+  });
 });
 
 
