3d04b600bd6ba0525c4d88c543322fd37e147be6
Dustin Goodman <dustin.s.goodman@gmail.com>
2021-09-06 14:52:56 -0400

    add location seeder

---
+++ server/seeders/20210906184940-AddLocations.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const locations = require('./locations.json');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Locations', locations, {});
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Locations', null, {});
+  }
+};
