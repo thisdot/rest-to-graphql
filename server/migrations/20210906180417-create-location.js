a1947ef796516d013af22890f9d51d2d448f2774
Dustin Goodman <dustin.s.goodman@gmail.com>
2021-09-06 14:13:03 -0400

    generate location model + migration

---
+++ server/migrations/20210906180417-create-location.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Locations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      type: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      dimension: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Locations');
+  }
+};
