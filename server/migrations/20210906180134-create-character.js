1c8561f6040c622df393f2b8448001a907c8446f
Dustin Goodman <dustin.s.goodman@gmail.com>
2021-09-06 14:12:50 -0400

    generate user model + migration

---
+++ server/migrations/20210906180134-create-character.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Characters', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      gender: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      status: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      species: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      type: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Characters');
+  }
+};
---
+++ server/models/character.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Character extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+
+    }
+  };
+  Character.init({
+    name: DataTypes.STRING,
+    image: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    status: DataTypes.STRING,
+    species: DataTypes.STRING,
+    type: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Character',
+  });
+  return Character;
+};
