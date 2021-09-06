e905399c06c1720d99f9744d16a064cbd7902033
Dustin Goodman <dustin.s.goodman@gmail.com>
2021-09-06 14:53:01 -0400

    add character seeder

---
+++ server/seeders/20210906184947-AddCharacters.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const characters = require('./characters.json');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Characters', characters, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Characters', null, {});
+  }
+};
