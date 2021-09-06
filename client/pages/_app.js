1bc43f03c04e5cee2fd33c333a355b819f0d7bc1
Dustin Goodman <dustin.s.goodman@gmail.com>
2021-09-06 15:54:34 -0400

    add apollo-client

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,9 +2,11 @@ import React, { useRef } from 'react';
 import { Helmet } from 'react-helmet';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { ApolloProvider } from '@apollo/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Hydrate } from 'react-query/hydration';
 
+import client from '../apollo-client.js';
 import { Container, SimpleGrid } from '../src/components/atoms';
 import { PageHeader } from '../src/components/organisms';
 import GlobalStyle from '../src/shared/global-style';
@@ -36,41 +38,43 @@ const App = ({ Component, pageProps }) => {
   }
 
   return (
-    <QueryClientProvider client={queryClientRef.current}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <CssBaseline />
-          <Helmet
-            htmlAttributes={{ lang: 'en' }}
-            title={defaultTitle}
-            link={[
-              {
-                rel: 'icon',
-                href: '/favicon.ico',
-              },
-            ]}
-            meta={[
-              {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1',
-              },
-              { property: 'og:title', content: defaultTitle },
-              {
-                name: 'description',
-                content: 'Rick and Morty universe information',
-              },
-            ]}
-          />
-          <SimpleGrid direction="column">
-            <PageHeader />
-            <Container>
-              <Component {...pageProps} />
-            </Container>
-          </SimpleGrid>
-        </ThemeProvider>
-      </Hydrate>
-    </QueryClientProvider>
+    <ApolloProvider client={client}>
+      <QueryClientProvider client={queryClientRef.current}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <CssBaseline />
+            <Helmet
+              htmlAttributes={{ lang: 'en' }}
+              title={defaultTitle}
+              link={[
+                {
+                  rel: 'icon',
+                  href: '/favicon.ico',
+                },
+              ]}
+              meta={[
+                {
+                  name: 'viewport',
+                  content: 'width=device-width, initial-scale=1',
+                },
+                { property: 'og:title', content: defaultTitle },
+                {
+                  name: 'description',
+                  content: 'Rick and Morty universe information',
+                },
+              ]}
+            />
+            <SimpleGrid direction="column">
+              <PageHeader />
+              <Container>
+                <Component {...pageProps} />
+              </Container>
+            </SimpleGrid>
+          </ThemeProvider>
+        </Hydrate>
+      </QueryClientProvider>
+    </ApolloProvider>
   );
 }
 
