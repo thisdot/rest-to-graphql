3024758a680072cfc248b28bc3ff6ad513ccbcaf
Jesse Tomchak <jtomchak@gmail.com>
2023-02-23 11:22:22 -0700

    find dotters with filters

--- apps/server/models/dotter/index.ts
+++ apps/server/models/dotter/index.ts
@@ -1,5 +1,5 @@
 export { getAll, getAllDotters } from "./getAll";
 export { create } from "./create";
-export { get } from "./get";
+export { get, getDotter } from "./get";
 export { update } from "./update";
 export { destroy } from "./destroy";
