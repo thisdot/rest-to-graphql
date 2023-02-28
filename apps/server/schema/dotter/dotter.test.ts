ee41d3efb48a9971f94270e23dd3725b4052daf6
Jesse Tomchak <jtomchak@gmail.com>
2023-02-28 12:10:47 -0700

    put and delete testing

--- apps/server/schema/dotter/dotter.test.ts
+++ apps/server/schema/dotter/dotter.test.ts
@@ -7,6 +7,7 @@ import {
 	mockDotterByIdGql,
 	mockNewDotter,
 } from "../../__mocks__/mockDotters";
+import { Dotter } from "@prisma/client";
 
 function assertSingleValue<TValue extends object>(
 	value: TValue | AsyncIterable<TValue>
@@ -90,6 +91,129 @@ describe("GQL Dotters", () => {
 			assertSingleValue(result);
 			const { firstName, lastName } = newMockDotter;
 			expect(result.data?.createDotter).toEqual({ firstName, lastName });
+		});
+	});
+	describe("Mutation update and existing Dotter", () => {
+		let dotterId: number;
+		it("returns a new Dotter", async () => {
+			const executor = buildHTTPExecutor({
+				fetch: yoga.fetch,
+			});
+			const newMockDotter = mockNewDotter();
+
+			const result = await executor({
+				document: parse(/* GraphQL */ `
+					mutation create($newMockDotter: CreateDotterInput) {
+						createDotter(dotter: $newMockDotter) {
+							id
+							firstName
+							lastName
+						}
+					}
+				`),
+				variables: { newMockDotter },
+			});
+			//@ts-ignore
+			dotterId = result.data?.createDotter.id;
+			assertSingleValue(result);
+			const { firstName, lastName } = newMockDotter;
+			expect(result.data.createDotter).toEqual(
+				expect.objectContaining({ firstName, lastName })
+			);
+		});
+
+		it("updates the dotters first name", async () => {
+			const executor = buildHTTPExecutor({
+				fetch: yoga.fetch,
+			});
+			const newFirstName = "Jesseeee";
+			const updatedDotter = await executor({
+				document: parse(/* GraphQL */ `
+					mutation updateDotter($id: Int!, $input: UpdateDotterInput) {
+						updateDotter(id: $id, dotter: $input) {
+							firstName
+							lastName
+							id
+						}
+					}
+				`),
+				variables: {
+					id: dotterId,
+					input: {
+						firstName: newFirstName,
+					},
+				},
+			});
+			assertSingleValue(updatedDotter);
+			expect(updatedDotter.data?.updateDotter.firstName).toEqual(newFirstName);
+		});
+	});
+	describe("Mutation deletes existing Dotter", () => {
+		let dotterId: number;
+		it("returns a new Dotter", async () => {
+			const executor = buildHTTPExecutor({
+				fetch: yoga.fetch,
+			});
+			const newMockDotter = mockNewDotter();
+
+			const result = await executor({
+				document: parse(/* GraphQL */ `
+					mutation create($newMockDotter: CreateDotterInput) {
+						createDotter(dotter: $newMockDotter) {
+							id
+							firstName
+							lastName
+						}
+					}
+				`),
+				variables: { newMockDotter },
+			});
+			//@ts-ignore
+			dotterId = result.data?.createDotter.id;
+			assertSingleValue(result);
+			const { firstName, lastName } = newMockDotter;
+			expect(result.data.createDotter).toEqual(
+				expect.objectContaining({ firstName, lastName })
+			);
+		});
+
+		it("removes dotter!", async () => {
+			const executor = buildHTTPExecutor({
+				fetch: yoga.fetch,
+			});
+			const deleteDotter = await executor({
+				document: parse(/* GraphQL */ `
+					mutation destroyDotter($id: Int!) {
+						deleteDotter(id: $id) {
+							firstName
+							lastName
+						}
+					}
+				`),
+				variables: {
+					id: dotterId,
+				},
+			});
+			assertSingleValue(deleteDotter);
+		});
+		it("return null when queried after deletion", async () => {
+			const executor = buildHTTPExecutor({
+				fetch: yoga.fetch,
+			});
+			const result = await executor({
+				document: parse(/* GraphQL */ `
+					query findDotter($dotterId: Int) {
+						findDotter(id: $dotterId) {
+							id
+							lastName
+							firstName
+						}
+					}
+				`),
+				variables: { dotterId },
+			});
+			assertSingleValue(result);
+			expect(result.data.findDotter).toEqual(null);
 		});
 	});
 });
